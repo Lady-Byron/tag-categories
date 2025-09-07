0b6a0cb7da2d0449a3c5c439f1bbb5aa6f92a299
Lady-Byron <samantha-everett@outlook.com>
2025-09-07 18:20:38 -0400

    Create SyncGroupTagsController.php

---
+++ src/Api/Controller/SyncGroupTagsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace LadyByron\TagCategories\Api\Controller;
+
+use Flarum\Api\Controller\AbstractShowController;
+use Flarum\Tags\Tag;
+use Flarum\User\Exception\PermissionDeniedException;
+use Flarum\User\User;
+use Illuminate\Support\Arr;
+use LadyByron\TagCategories\Api\Serializer\TagCategoryGroupSerializer;
+use LadyByron\TagCategories\Repository\TagCategoryGroupRepository;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class SyncGroupTagsController extends AbstractShowController
+{
+    public $serializer = TagCategoryGroupSerializer::class;
+
+    protected $groups;
+
+    public function __construct(TagCategoryGroupRepository $groups)
+    {
+        $this->groups = $groups;
+    }
+
+    protected function data(Request $request, $document)
+    {
+        /** @var User $actor */
+        $actor = $request->getAttribute('actor');
+        if (!$actor->isAdmin()) {
+            throw new PermissionDeniedException();
+        }
+
+        $id = (int) $request->getQueryParams()['id'];
+        $group = $this->groups->findOrFail($id);
+
+        // 期望 payload: { data: { attributes: { tagIds: [1,2,3] } } }
+        $tagIds = Arr::get($request->getParsedBody(), 'data.attributes.tagIds', []);
+        $tagIds = array_values(array_unique(array_map('intval', $tagIds)));
+
+        // 只允许同步现有标签
+        $validIds = Tag::query()->whereIn('id', $tagIds)->pluck('id')->all();
+        $group->tags()->sync($validIds);
+
+        return $group->load('tags');
+    }
+}
