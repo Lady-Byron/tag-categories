91173a9de3ed4d2384cd4ffec69d1a124c2c4faa
Lady-Byron <samantha-everett@outlook.com>
2025-09-07 18:17:45 -0400

    Create ListTagCategoryGroupsController.php

---
+++ src/Api/Controller/ListTagCategoryGroupsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LadyByron\TagCategories\Api\Controller;
+
+use Flarum\Api\Controller\AbstractListController;
+use Flarum\User\User;
+use LadyByron\TagCategories\Api\Serializer\TagCategoryGroupSerializer;
+use LadyByron\TagCategories\Repository\TagCategoryGroupRepository;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Tobscure\JsonApi\Document;
+
+class ListTagCategoryGroupsController extends AbstractListController
+{
+    public $serializer = TagCategoryGroupSerializer::class;
+
+    protected $groups;
+
+    public function __construct(TagCategoryGroupRepository $groups)
+    {
+        $this->groups = $groups;
+    }
+
+    protected function data(Request $request, Document $document)
+    {
+        // Forum 端允许只读
+        /** @var User $actor */
+        $actor = $request->getAttribute('actor');
+
+        // include=tags 时联表返回
+        $include = $this->extractInclude($request);
+        $query = $this->groups->allOrdered();
+        if (in_array('tags', $include, true)) {
+            $query->load('tags');
+        }
+
+        return $query;
+    }
+}
